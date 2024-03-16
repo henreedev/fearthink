de5cdd544d466c63c4d4f6bedc35331716435bb6
bobertsanchezez <henry_earnest@brown.edu>
2024-03-16 12:57:03 -0400

    npc selection

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,7 +6,10 @@ var drag_dist := 30.0
 var pan_speed := 50.0
 var level_size : Vector2 
 @onready var game : Game = get_tree().get_first_node_in_group("game")
-
+@onready var selector : Area2D = $Selector
+var selected_npcs = []
+var selected_npc : NPC 
+var selection_range := 50.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -15,8 +18,10 @@ func _act_on_state(delta):
 	match state:
 		State.PLANNING:
 			_move_screen_at_edge(delta)
+			_update_selector()
 		State.READY:
 			_move_screen_at_edge(delta)
+			_update_selector()
 		State.TRAVELLING:
 			pass
 		State.INHABITING:
@@ -30,6 +35,33 @@ func _move_screen_at_edge(delta):
 		var extra_speed = mag / drag_dist
 		position = position.move_toward(position + mouse_pos, extra_speed * pan_speed * delta)
 	clamp_pos_to_level_dims()
+
+func _update_selector():
+	var mouse_pos := get_local_mouse_position()
+	selector.position = mouse_pos
+	var best_npc : NPC
+	var best_flashing_npc : NPC 
+	var best_dist := 999999.0
+	var best_flashing_dist := 999999.0
+	for npc : NPC in selected_npcs:
+		var flashing = npc.state == NPC.State.COWERING
+		#Conditions to select: 
+		#1. must be in range 
+		#2. prioritize flashing
+		var dist = npc.global_position.distance_squared_to(selector.global_position)
+		if dist < best_dist:
+			best_dist = dist
+			best_npc = npc
+		if flashing and dist < best_flashing_dist:
+			best_flashing_dist = dist
+			best_flashing_npc = npc
+	if best_flashing_npc:
+		selected_npc = best_flashing_npc 
+	else:
+		selected_npc = best_npc if best_npc else null
+	if selected_npc:
+		selected_npc.sprite.speed_scale = 10.0
+	
 
 func clamp_pos_to_level_dims():
 	# limit position to screen edge
@@ -45,4 +77,18 @@ func clamp_pos_to_level_dims():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	for npc : NPC in get_tree().get_nodes_in_group("npc"):
+		npc.sprite.speed_scale = 1.0 # TODO remove
 	_act_on_state(delta)
+
+
+func _on_selector_area_entered(area):
+	if area is NPC:
+		selected_npcs.append(area)
+		print("added")
+
+
+func _on_selector_area_exited(area):
+	if area is NPC:
+		selected_npcs.erase(area)
+		print("removed")
